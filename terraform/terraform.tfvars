936ae621009bb8ec13088bcc64e2fef3a68fc516
masaaya <156692516+mkanetsuna@users.noreply.github.com>
2025-09-21 07:40:50 +0000

    feat: parameterize github branch protection branch

--- terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-# Copy this file to terraform.tfvars or provide variables via CLI/environment.
-github_owner = "mkanetsuna"
-repository_name = "zenn-contents"
-required_approving_review_count = 0
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,11 +7,6 @@ variable "repository_name" {
   type        = string
 }
 
-variable "required_approving_review_count" {
-  description = "Number of approving reviews required before merging."
-  type        = number
-}
-
 variable "branch_pattern" {
   description = "Branch name pattern to apply protections to."
   type        = string
