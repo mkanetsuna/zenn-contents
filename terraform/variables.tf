359f232bfdddebee529a184238906de266f00e1c
masaaya <156692516+mkanetsuna@users.noreply.github.com>
2025-09-21 07:15:17 +0000

    chore: drop default repo settings from terraform vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,11 @@
 variable "github_owner" {
   description = "GitHub organization or user that owns the repository."
   type        = string
-  default     = "mkanetsuna"
 }
 
 variable "repository_name" {
   description = "Repository name to apply branch protection to."
   type        = string
-  default     = "zenn-contents"
 }
 
 variable "required_approving_review_count" {
