5be4533d8f30304e7f40e5ddf346f8911c1067dc
masaaya <156692516+mkanetsuna@users.noreply.github.com>
2025-09-21 07:43:28 +0000

    branch name default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,9 +5,15 @@ variable "github_owner" {
 variable "repository_name" {
   description = "Repository name to apply branch protection to."
   type        = string
+  default     = "main"
 }
 
 variable "branch_pattern" {
   description = "Branch name pattern to apply protections to."
   type        = string
+}
+variable "branch_pattern" {
+  description = "Branch name pattern to apply protections to."
+  type        = string
+  default     = "main"
 }
