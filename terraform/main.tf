1bab8b2c9d0f99872feca0a62795a38c71c21e99
masaaya <156692516+mkanetsuna@users.noreply.github.com>
2025-09-21 07:40:50 +0000

    feat: parameterize github branch protection branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,18 +8,18 @@ data "github_repository" "target" {
 
 resource "github_branch_protection" "main" {
   repository_id = data.github_repository.target.node_id
-  pattern        = var.branch_pattern
+  pattern       = var.branch_pattern
 
-  enforce_admins        = true
-  allows_force_pushes   = false
-  allows_deletions      = false
-  lock_branch           = false
-  require_signed_commits = true
+  enforce_admins                  = true
+  allows_force_pushes             = false
+  allows_deletions                = false
+  lock_branch                     = false
+  require_signed_commits          = true
   require_conversation_resolution = true
 
   required_pull_request_reviews {
-    dismiss_stale_reviews      = true
-    require_code_owner_reviews = false
-    required_approving_review_count = var.required_approving_review_count
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = false
+    required_approving_review_count = 0
   }
 }
